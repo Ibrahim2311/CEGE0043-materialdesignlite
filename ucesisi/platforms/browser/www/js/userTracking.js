bbcf6fb5b8710a797c9b018ce2a0f4de2fbcb9ca
Ibrahim Alhadidi <i.s.h.94@hotmail.com>
2019-03-07 13:42:51 -0500

    removing old tracking markers

--- ucesisi/platforms/browser/www/js/userTracking.js
+++ ucesisi/platforms/browser/www/js/userTracking.js
@@ -2,7 +2,13 @@ function trackLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.watchPosition(showPosition);        
     }
+}
 
+var userMarker
 function showPosition(position) {
-    L.marker([position.coords.latitude, position.coords.longitude]).addTo(mymap).bindPopup("<b>You were here</b>");
+    if (userMarker){
+        mymap.removeLayer(userMarker);
+        }
+    userMarker = L.marker([position.coords.latitude, position.coords.longitude]).addTo(mymap).bindPopup("<b>You were here</b>");
+    mymap.flyTo([position.coords.latitude, position.coords.longitude], 18)
 }
